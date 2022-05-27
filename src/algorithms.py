56a47256f54fa4ae6c485e84ea398b3bdbbe0ce2
Pat WSL <pat.hearps@bluescope.com>
2022-05-27 15:27:01 +1000

    FEAT: first algorithm

---
+++ src/algorithms.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def index_find_max_consec_nulls(series: pd.Series) -> int:
+    """Series should be raw data with nulls, without pd.isnull() applied"""
+    return int(pd.Series(series.dropna().index).diff().max())
+
+
+def _index_find_max_consec_nulls(series: pd.Series) -> int:
+    """Separate logical steps of index_find_max_consec_nulls"""
+    with_sequential_index = series.reset_index(drop=True)
+    only_non_null_indices = with_sequential_index.dropna().index
+    series_non_null_indices = pd.Series(only_non_null_indices)
+    size_of_null_gaps = series_non_null_indices.diff() - 1
+    largest_null_gap = int(size_of_null_gaps.max())
+    return largest_null_gap
