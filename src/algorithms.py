fcfc0878878f4b3a3f531adb5c6fdd975e9e9fb9
Pat WSL <pat.hearps@bluescope.com>
2022-05-27 15:33:07 +1000

    FIX: deal with all/none edge cases

--- src/algorithms.py
+++ src/algorithms.py
@@ -8,9 +8,15 @@ def index_find_max_consec_nulls(series: pd.Series) -> int:
 
 def _index_find_max_consec_nulls(series: pd.Series) -> int:
     """Separate logical steps of index_find_max_consec_nulls"""
-    with_sequential_index = series.reset_index(drop=True)
-    only_non_null_indices = with_sequential_index.dropna().index
-    series_non_null_indices = pd.Series(only_non_null_indices)
-    size_of_null_gaps = series_non_null_indices.diff() - 1
-    largest_null_gap = int(size_of_null_gaps.max())
-    return largest_null_gap
+    if series.notnull().all():
+        answer = 0
+    elif series.isnull().all():
+        answer = len(series)
+    else:
+        with_sequential_index = series.reset_index(drop=True)
+        only_non_null_indices = with_sequential_index.dropna().index
+        series_non_null_indices = pd.Series(only_non_null_indices)
+        size_of_null_gaps = series_non_null_indices.diff() - 1
+        largest_null_gap = int(size_of_null_gaps.max())
+        answer = largest_null_gap
+    return answer
