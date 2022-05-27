b9c1678d297d17564d3aad6fb53979f595c792ca
Pat WSL <pat.hearps@bluescope.com>
2022-05-27 15:48:15 +1000

    DOCS: explain func a little more

--- src/algorithms.py
+++ src/algorithms.py
@@ -19,7 +19,7 @@ def index_find_max_consec_nulls(series: pd.Series) -> int:
 
 
 def _index_find_max_consec_nulls(series: pd.Series) -> int:
-    """Separate logical steps of index_find_max_consec_nulls"""
+    """Separated logical steps of index_find_max_consec_nulls()"""
     if series.notnull().all():
         answer = 0
     elif series.isnull().all():
@@ -29,6 +29,7 @@ def _index_find_max_consec_nulls(series: pd.Series) -> int:
         with_sequential_index = series.reset_index(drop=True)
         only_non_null_indices = with_sequential_index.dropna().index
         series_non_null_indices = pd.Series(only_non_null_indices)
+        # minus 1 because we only want size of gap between indices
         size_of_null_gaps = series_non_null_indices.diff() - 1
         largest_null_gap = int(size_of_null_gaps.max())
         answer = largest_null_gap
